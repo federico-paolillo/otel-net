f5f64454f99372b05984b66d7aa6f9861e95b637
Federico Paolillo <federico.paolillo@adesso.ch>
2024-10-16 10:58:45 +0200

    feat: configured simple logger in console

--- Tel.Web/Program.cs
+++ Tel.Web/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.Extensions.Logging.Console;
+
 using Tel.Weather;
 using Tel.Web.Endpoints.Weather;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddWeatherServices();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSimpleConsole(opts =>
+{
+    opts.SingleLine = true;
+    opts.IncludeScopes = false;
+    opts.UseUtcTimestamp = true;
+    opts.TimestampFormat="yyyy-MM-dd HH:mm:ss";
+    opts.ColorBehavior = LoggerColorBehavior.Disabled;
+});
 
 WebApplication app = builder.Build();
 
